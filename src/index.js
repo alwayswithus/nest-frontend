b2566dcdd58d63a0bb7bea024924a7466eb03944
hur-gilhaeng <hgh8401@gmail.com>
2020-05-08 10:16:20 +0900

    login add

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Dashboard from './dashboard/Dashboard';
+import Login from './login/Login'
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Dashboard />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
