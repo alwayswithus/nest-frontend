5054c4d0c3c8f178645f8712c7e9dcb09af5dc24
Kim-woo-gyeong <lovelykwg@naver.com>
2020-05-08 17:42:08 +0900

    0508/김우경/프로필계정설정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component, Fragment } from "react";
 import { Router, Route } from "react-router-dom";
 
-import Home from './Home';
+import Setting from './tasksetting/Setting';
 import File from './file/File';
 import { BrowserRouter } from "react-router-dom";
 
 import Comment from './comment/Comment';
-import ProfileMain from './profile/ProfileMain';
 import KanbanMain from "./kanban/KanbanMain";
 import Login from "./login/Login";
 import Dashboard from "./dashboard/Dashboard";
 import Profile from './profile/Profile';
-
+import ProfileSetting from './profile/ProfileSetting';
 
 function App() {
   return (
@@ -24,9 +23,9 @@ function App() {
 
         {/* 프로필설정 */}
         <Route path="/profile" exact component={Profile} />
-
+        <Route path="/profile/setting" exact component={ProfileSetting} />
         {/* 업무속성 */}
-        <Route path="/home" exact component={Home} />
+        <Route path="/home" exact component={Setting} />
         <Route path="/comment" exact component={Comment} />
         <Route path="/file" exact component={File} />
 
@@ -36,8 +35,6 @@ function App() {
         {/* 대시보드 */}
         <Route path="/dashboard" exact component={Dashboard} />
 
-
-        <ProfileMain />
       </BrowserRouter>
 
     </div>
---
+++ src/profile/Test.js
@@ -0,0 +1,88 @@
+import React,{useState} from 'react';
+
+const Test = (props) => {
+    
+    const [visible, setVisible] = useState(true);
+    
+    // const FoldEvent = () => {
+    //    setVisible(!visible);
+    // }
+
+    const onChanged = (e) => {
+        console.log(e.target.a);
+        // const visible = e.target.className === 'fa-arrow-circle-down';
+        setVisible(visible);
+    }
+    return (
+        <div class="panel-group" id="accordion">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"> 비밀번호 </a>
+                </h4>
+                </div>
+                <div id="collapse1" class="panel-collapse collapse in">
+                <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.</div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse2"> 시간 설정 </a>
+                </h4>
+                </div>
+                <div id="collapse2" class="panel-collapse collapse">
+                <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.</div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse3"> 언어 설정 </a>
+                </h4>
+                </div>
+                <div id="collapse3" class="panel-collapse collapse">
+                <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.</div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a onClick={onChanged} data-toggle="collapse" data-parent="#accordion" href="#collapse4">알림 설정 </a>
+                    
+                </h4>
+                </div>
+                <div id="collapse4" class="panel-collapse collapse">
+                <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.</div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                <h4 class="panel-title">
+                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">계정 삭제 </a>
+                </h4>
+                </div>
+                <div id="collapse5" class="panel-collapse collapse">
+                <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.</div>
+                </div>
+            </div>
+        </div>
+    )
+ }
+
+ export default Test;
