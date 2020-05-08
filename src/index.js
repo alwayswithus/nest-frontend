7cf1374acccc5c5057759ed6fea5f55b3cdf6eed
inhyo <inhyo825@gmail.com>
2020-05-08 11:38:15 +0900

    css수정중

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Dashboard from "./dashboard/Dashboard";
 import KanbaMain from "./kanban/KanbaMain";
+import Login from "./login/Login";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
