0c9c8dbfd0c5d9a65a5cf4e097f51ea764c64158
haeyong Hahn <youg1322@naver.com>
2020-05-15 18:00:16 +0900

    0515 / 한해용 / dashboard 및 kanban scroll 수정

--- src/dashboard/projectsetting/ProjectHeader.js
+++ src/dashboard/projectsetting/ProjectHeader.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import './Projectheader.scss';
 
 class ProjectHeader extends Component  {
-    render(){
+    
+    render() {
         return (
             <div className="ProjectHeader">
+                <button type="button" className="close"><i class="fas fa-times fa-1x"></i></button>
                 <div className="Header-list">
                     <h2><b>Project Name</b></h2>
                     <span>작성자 : {this.props.name} • &nbsp;&nbsp;&nbsp; 작성일 : {this.props.date}</span>
