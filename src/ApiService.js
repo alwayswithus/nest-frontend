4b10ea87ed0a61a02b65a62554e9bcdef54b4729
hur-gilhaeng <hgh8401@gmail.com>
2020-06-29 12:58:47 +0900

    0629 / 허길행 / 네비게이터 관련 수정

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const DASHBOARD_API_URL = "http://localhost:8080/nest";
+// const DASHBOARD_API_URL = "http://localhost:8080/nest";
 
 class ApiService {
     fetchDashboard() {
